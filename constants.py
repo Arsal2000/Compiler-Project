7469c412bdcee5bad676d013c2ec7128ff66e2b6
Muhammad Arsal <arsal7056@gmail.com>
2023-09-09 18:58:24 +0500

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,34 @@
+TT_INT				= 'INTEGER'
+TT_FLOAT    	= 'FLOAT'
+TT_IDENTIFIER	= 'IDENTIFIER'
+TT_KEYWORD		= 'KEYWORD'
+TT_PLUS     	= 'PLUS'
+TT_MINUS    	= 'MINUS'
+TT_MUL      	= 'MUL'
+TT_DIV      	= 'DIV'
+TT_POW				= 'POW'
+TT_EQ					= 'EQ'
+TT_LPAREN   	= 'LPAREN'
+TT_RPAREN   	= 'RPAREN'
+TT_EE					= 'EE'
+TT_NE					= 'NE'
+TT_LT					= 'LT'
+TT_GT					= 'GT'
+TT_LTE				= 'LTE'
+TT_GTE				= 'GTE'
+TT_EOF				= 'EOF'
+
+KEYWORDS = [
+	'VARIABLE',
+	'AND',
+	'OR',
+	'NOT',
+	'IF',
+	'ELIF',
+	'ELSE',
+	'FOR',
+	'TO',
+	'STEP',
+	'WHILE',
+	'THEN'
+]
